594f0d72e8a827057a5bed55ea13da5b2c120853
Rajesh-Dell <rajeshsurunga@gmail.com>
2016-10-19 21:10:46 -0400

    chhange frag

---
+++ MyFragment2.java
@@ -0,0 +1,20 @@
+package com.exercise.FragmentTest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment2 extends Fragment {
+
+	 @Override
+	 public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	   Bundle savedInstanceState) {
+	  // TODO Auto-generated method stub
+	  View myFragmentView = inflater.inflate(R.layout.fragmentlayout2, container, false);
+	  
+	  return myFragmentView;
+	 }
+
+}
